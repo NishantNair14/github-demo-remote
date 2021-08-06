e8a8340e6d3275da1ff9c3f62808ae180601ce08
NishantNair14 <55617663+NishantNair14@users.noreply.github.com>
2021-08-06 14:10:31 +0530

    Math.py added

--- math.py
+++ math.py
@@ -1,17 +1,12 @@
 #add implementation
 def add(x,y):
-<<<<<<< HEAD
-     return x+y    #replace with return
-=======
-     pass
->>>>>>> parent of 7234a1a... added instruction to replace pass keyword with return
-
+     return x+y    
 #subtract implementation	 
 def sub(x,y):
-     pass	       #replace with return
+    return x-y	     
 #multiply implementation
 def mul(x,y):
-     pass
+     return x*y
 #divide implementation
 def div(x,y):
      return x/y   	 
