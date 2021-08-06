5878fdd94f65f717d280032c38192177b90dfb1f
NishantNair14 <55617663+NishantNair14@users.noreply.github.com>
2021-08-06 13:28:21 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#add implementation
+def add(x,y):
+<<<<<<< HEAD
+     return x+y    #replace with return
+=======
+     pass
+>>>>>>> parent of 7234a1a... added instruction to replace pass keyword with return
+
+#subtract implementation	 
+def sub(x,y):
+     pass	       #replace with return
+#multiply implementation
+def mul(x,y):
+     pass
+#divide implementation
+def div(x,y):
+     return x/y   	 
+	 
+	 
